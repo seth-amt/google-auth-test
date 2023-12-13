d121858612cc4da7aa8c4ee32ae6df3a4fc67c3b
seth-amt <152368511+seth-amt@users.noreply.github.com>
2023-12-13 11:51:14 +0000

    added login with google integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,29 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'google-auth-test';
+  title = 'Google Auth';
+
+  async handleCredentialResponse(response: any) {
+    // Here will be your response from Google.
+    console.log(response);
+  }
+
+  ngOnInit() {
+    // @ts-ignore
+    google.accounts.id.initialize({
+      client_id: "",
+      callback: this.handleCredentialResponse.bind(this),
+      auto_select: false,
+      cancel_on_tap_outside: true,
+  
+    });
+    // @ts-ignore
+    google.accounts.id.renderButton(
+    // @ts-ignore
+    document.getElementById("google-button"),
+      { theme: "outline", size: "large", width: "100%" }
+    );
+    // @ts-ignore
+    google.accounts.id.prompt((notification: PromptMomentNotification) => {});
+  } 
 }
